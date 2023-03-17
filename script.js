5a5f36ea7d43db627b8b69d4e605cb54a9c4ef13
Nazarreto <102985652+Nazarreto@users.noreply.github.com>
2023-03-17 10:23:56 +0200

    create

---
+++ script.js
@@ -0,0 +1,52 @@
+///* task 1
+
+// let qwe = prompt("write some text");
+
+// let space = 0;
+
+// let pos = qwe.indexOf(" ");
+
+// while (pos !== -1) {
+//   pos = qwe.indexOf(" ", pos + 1);
+//   space++;
+// }
+// alert(space);
+
+// * task 2
+
+// let email = prompt(`Enter your email`);
+// let check = email.indexOf("@");
+
+// if (check == -1 || check == 0 || check == email.length - 1) {
+//   alert("Email невалідний!");
+// } else {
+//   alert("Email валідний!");
+// }
+
+// *task 3
+
+// let qwe = prompt("write some text");
+
+// let count = 0;
+
+// let word = qwe.indexOf("html");
+
+// while (word !== -1) {
+//   word = qwe.indexOf("html", word + 1);
+//   count++;
+// }
+// console.log(count);
+
+// *task 4
+
+// let qwe = prompt("Введіть посилання");
+// switch (true) {
+//   case qwe.includes("https://"):
+//     console.log(qwe.slice(8));
+//     break;
+//   case qwe.includes("http://"):
+//     console.log(qwe.slice(7));
+//     break;
+//   default:
+//     break;
+// }
